c116e0c12b0f69a95f4bd6027574d3a55046aba7
FMGordillo <facundomgordillo@gmail.com>
2016-10-02 16:49:01 -0300

    Primer Login

--- mobile/src/main/java/com/example/fmgordillo/clases/Login.java
+++ mobile/src/main/java/com/example/fmgordillo/clases/Login.java
@@ -1,13 +1,27 @@
 package com.example.fmgordillo.clases;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        
+        login = (Button) findViewById(R.id.login_btn_accept);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this, Menu.class));
+            }
+        });
     }
 }
