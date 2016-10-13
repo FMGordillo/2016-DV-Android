ddb8b23fa1834c08815aee078e298e221264f325
FMGordillo <facundomgordillo@gmail.com>
2016-10-13 12:06:49 -0300

    About Me, y con eso terminamos

--- mobile/src/main/java/com/example/fmgordillo/clases/AboutMe.java
+++ mobile/src/main/java/com/example/fmgordillo/clases/AboutMe.java
@@ -2,22 +2,23 @@ package com.example.fmgordillo.clases;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class AboutMe extends Activity {
 
-    TextView description;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tabs_aboutme);
-
         inicializar();
+
 
     }
 
     private void inicializar() {
-        description = (TextView) findViewById(R.id.aboutme_description);
+
     }
+
+
 }
--- mobile/src/main/java/com/example/fmgordillo/clases/Menu.java
+++ mobile/src/main/java/com/example/fmgordillo/clases/Menu.java
@@ -4,10 +4,12 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TabHost;
+import android.widget.TextView;
 
 public class Menu extends Activity {
 
@@ -16,6 +18,25 @@ public class Menu extends Activity {
     EditText nombre, edad;
     Button ABMbtn, settingsBtn;
     boolean a = false;
+    TextView description;
+    StringBuilder sb;
+
+    @Override
+    public boolean onCreateOptionsMenu(android.view.Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_settings, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_settings:
+                startActivity(new Intent(Menu.this, Settings.class));
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +48,9 @@ public class Menu extends Activity {
         change = (Button) findViewById(R.id.menu_changeColor_btn);
         stayIdle = (Button) findViewById(R.id.menu_changeColorDefault_btn);
         ABMbtn = (Button) findViewById(R.id.tabs_3_alta_btn);
-        settingsBtn = (Button) findViewById(R.id.menu_changeSettings_btn);
+        //settingsBtn = (Button) findViewById(R.id.menu_changeSettings_btn);
+        description = (TextView) findViewById(R.id.aboutme_description);
+        sb = new StringBuilder();
         tabs.setup();
 
         // *** TAB 1
@@ -58,7 +81,7 @@ public class Menu extends Activity {
         // *** TAB 2
         spec = tabs.newTabSpec("ABM");
         spec.setContent(R.id.tab2);
-        spec.setIndicator("TAB2");
+        spec.setIndicator("ABMC");
         ABMbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -70,15 +93,29 @@ public class Menu extends Activity {
         // *** TAB 3
         spec = tabs.newTabSpec("About me");
         spec.setContent(R.id.tab3);
-        spec.setIndicator("TAB3");
+        spec.setIndicator("Sobre mi");
         tabs.addTab(spec);
-        settingsBtn.setOnClickListener(new View.OnClickListener() {
+        description.setText(setAndShowSB(sb));
+        /*settingsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(Menu.this, Settings.class));
             }
-        });
+        });*/
         tabs.setCurrentTab(0);
 
     }
+
+    private String setAndShowSB(StringBuilder sb) {
+        sb.append("Nombre: Facundo Gordillo");
+        sb.append("\n");
+        sb.append("Edad: 21 años");
+        sb.append("\n");
+        sb.append("Motivo de aplicacion: Antes de ponerme a desarrollar" +
+                " con tecnologías complicadas, o con el proyecto, quería" +
+                " darme un pequeño pantallazo de lo que sería desarrollar" +
+                " en Android.");
+        return sb.toString();
+    }
+
 }
